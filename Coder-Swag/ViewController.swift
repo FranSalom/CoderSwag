14a9484dd70bd06c8db3c0cdd9de93a8bbdc8a5a
Francisco Salom <FranSalom@MBPdeFrancisco.cantv.net>
2017-10-05 18:54:10 -0400

    Creacion del proyecto

---
+++ Coder-Swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by Francisco Salom on 5/10/17.
+//  Copyright © 2017 Francisco Salom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
